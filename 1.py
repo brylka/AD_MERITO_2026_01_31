36d90848c4e966233dd1f332d5a1efbf598905f0
Bartosz Bryniarski <brylka@jelenia.com>
2026-01-31 11:17:54 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 from tensorflow import keras
+import matplotlib.pyplot as plt
 
 X = np.array([[0,0], [0,1], [1,0], [1,1]])
 y = np.array([0,0,0,1])
@@ -19,7 +20,7 @@ model.compile(
     metrics=['accuracy']
 )
 
-model.fit(X, y, epochs=100, verbose=1)
+history = model.fit(X, y, epochs=100, verbose=1)
 
 predyction = model.predict(X)
 print("Predykcja:")
@@ -27,3 +28,15 @@ for i in range(len(X)):
     x = X[i]
     pred = predyction[i]
     print(f"{x[0]} AND {x[1]} = {pred[0]:.4f} -> {int(pred[0] > 0.5)}")
+
+plt.figure(figsize=(10,5))
+
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'])
+plt.grid(True)
+
+plt.subplot(1,2,2)
+plt.plot(history.history['accuracy'])
+plt.grid(True)
+
+plt.show()
