77bb592b43f28e7d3a8b8595bf4a034d77c89a87
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-01 10:24:18 +0100

    Niedziela

--- 3.py
+++ 3.py
@@ -13,6 +13,9 @@ print(f"Dane testowe: {x_test.shape}")
 model = keras.Sequential([
     layers.Flatten(input_shape=(28,28)),
 
+    layers.Dense(256, activation='relu'),
+    layers.Dropout(0.2),
+
     layers.Dense(128, activation='relu'),
     layers.Dropout(0.2),
 
